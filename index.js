144eed838cabc1f171a879db9ea9911e171389c0
rafaelpbar <rafael.coper@outlook.com>
2021-04-19 15:46:20 -0300

    Up

--- index.js
+++ index.js
@@ -1,28 +1,27 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const path = require('path');
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
 
-const userController = require('./controller/userController');
-const loginController = require('./controller/loginController');
-const recipeController = require('./controller/recipeController');
+const userController = require("./controller/userController");
+const loginController = require("./controller/loginController");
+const recipeController = require("./controller/recipeController");
 
 const app = express();
 
 app.use(express.json());
-app.use(bodyParser.json());
 
-app.use('/users', userController);
+app.use("/users", userController);
 
-app.use('/login', loginController);
+app.use("/login", loginController);
 
-app.use('/recipes', recipeController); // algo aqui deu ruim
+app.use("/recipes", recipeController); // algo aqui deu ruim
 
-app.use('/images', express.static(path.join(__dirname, 'image')));
+app.use("/images", express.static(path.join(__dirname, "image")));
 
-app.get('/', (_request, response) => {
+app.get("/", (_request, response) => {
   response.send();
 });
 
 app.listen(3000, () => {
-  console.log('Listening on port 3000');
+  console.log("Listening on port 3000");
 });
